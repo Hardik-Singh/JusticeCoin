abcf459b9a395bd214a4c379274c43746ff83a65
Hardik-Singh <68667083+Hardik-Singh@users.noreply.github.com>
2022-09-22 16:59:19 -0500

    fixed link
    link

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './styles.css';
 import { BrowserRouter as Router, Route, Switch, withRouter } from "react-router-dom";
 import HomePage from "./Home/HomePage";
 import FAQPage from "./FAQ/FAQPage";
-import AboutPage from "./About/AboutPage";
 import WebMiner from "./WebMiner/WebMinerPage";
 import DownloadPage from "./Download/DownloadPage";
 import PageNotFound from "./PageNotFound";
@@ -26,7 +25,6 @@ function App() {
         {/* WebSite Routes */}
         <Route path="/" exact component={withRouter(HomePage)} />
         <Route path="/faq" exact component={withRouter(FAQPage)} />
-        <Route path="/about" exact component={withRouter(AboutPage)} />
         <Route path="/webminer" exact component={withRouter(WebMiner)} />
         <Route path="/download" exact component={withRouter(DownloadPage)} />
         <Route path="/download/mac" exact render={withRouter(props => <DownloadPage {...props} OS="Mac OS" />)} />
